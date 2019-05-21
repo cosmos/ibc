1985fa963cf1a020f148fbdd8e090e9857409bdd
Christopher Goes <cwgoes@pluranimity.org>
2019-05-21 16:29:19 +0200

    Check Go syntax too

--- scripts/check-syntax.sh
+++ scripts/check-syntax.sh
@@ -4,5 +4,11 @@ set -e
 
 for file in $(find . -type f -name "*.md"); do
   echo "Checking syntax in $file..."
+  tempfile=$(mktemp).go
+  echo -e "package main\n\n" > $tempfile
+  cat $file | codedown golang >> $tempfile
+  echo -e "\n\nfunc main() {}\n" >> $tempfile
+  go run $tempfile || (cat $tempfile && rm -f $tempfile && exit 1)
+  rm -f $tempfile
   cat $file | codedown coffeescript | coffee -s
 done
