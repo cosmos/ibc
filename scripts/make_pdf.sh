44873d29028caa5fe2eadf1a4de679c4ab1e6272
Christopher Goes <cwgoes@pluranimity.org>
2019-08-13 16:51:59 +0200

    Update PDF script & PDF

--- scripts/make_pdf.sh
+++ scripts/make_pdf.sh
@@ -7,4 +7,5 @@ find $DIR -type f -name "*.md" -exec cp {} {}.xfm \;
 find $DIR -type f -name "*.md.xfm" -exec awk -i inplace '/## Backwards Compatibility/ {exit} {print}' {} \;
 find $DIR -type f -name "*.png" -exec cp {} . \;
 pandoc --pdf-engine=xelatex --template eisvogel --filter pandoc-include --mathjax --toc --number-sections -o spec.pdf spec.pdc
+find $DIR -type f -name "*.md.xfm" -exec rm {} \;
 rm *.png
