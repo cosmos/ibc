25ccb83ad305129db028809937b4d8877f876151
Christopher Goes <cwgoes@pluranimity.org>
2019-08-17 16:59:55 +0200

    Further PDF cleanup

--- scripts/make_pdf.sh
+++ scripts/make_pdf.sh
@@ -3,9 +3,22 @@
 set -xe
 
 DIR=./spec
+DIR2=./ibc
+
+# preprocessing
+
 find $DIR -type f -name "*.md" -exec cp {} {}.xfm \;
 find $DIR -type f -name "*.md.xfm" -exec awk -i inplace '/## Backwards Compatibility/ {exit} {print}' {} \;
 find $DIR -type f -name "*.png" -exec cp {} . \;
-pandoc --pdf-engine=xelatex --template eisvogel --filter pandoc-include --mathjax --toc --number-sections -o spec.pdf spec.pdc
+find $DIR2 -type f -name "*.md" -exec cp {} {}.xfm \;
+find $DIR2 -type f -name "*.md.xfm" -exec awk -i inplace '/^##/{p=1}p' {} \;
+
+# pdf generation
+
+pandoc --pdf-engine=xelatex --template eisvogel --filter pandoc-include --mathjax --toc --number-sections -t latex -o spec.pdf spec.pdc
+
+# cleanup
+
 find $DIR -type f -name "*.md.xfm" -exec rm {} \;
+find $DIR2 -type f -name "*.md.xfm" -exec rm {} \;
 rm *.png
