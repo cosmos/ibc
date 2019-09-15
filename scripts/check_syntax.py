6a5fb4f5a36cd0de1f1258c924381c429543f12d
Christopher Goes <cwgoes@pluranimity.org>
2019-09-15 23:53:43 +0200

    Merge PR #259: Move CONSENSUS.md into ICS 2

--- scripts/check_syntax.py
+++ scripts/check_syntax.py
@@ -4,6 +4,7 @@ import re, os, sys, subprocess, glob
 
 specs = [f.path for f in os.scandir('./spec') if f.is_dir()]
 files = [f.path for spec in specs for f in os.scandir(spec) if f.is_file() and f.path[-3:] == '.md']
+files = sorted(files)
 
 requires_regex = re.compile('requires: (.*)\n')
 
@@ -24,26 +25,27 @@ for fn in files:
     if output != b'':
         sys.exit(1)
 
-    print('Reading dependencies of {}'.format(fn))
-    data = open(fn).read()
-    requires = [int(num) for line in requires_regex.findall(data) for num in line.split(', ')]
+    if 'ics-001' not in fn:
+        print('Reading dependencies of {}'.format(fn))
+        data = open(fn).read()
+        requires = [int(num) for line in requires_regex.findall(data) for num in line.split(', ')]
 
-    print('Dependencies: {}'.format(requires))
-    final = temp_filename()
+        print('Dependencies: {}'.format(requires))
+        final = temp_filename()
 
-    for dep in requires:
-        spec = glob.glob('./spec/ics-' + str(dep).zfill(3) + '-*')[0] + '/README.md'
-        newTemp = extract_typescript(spec)
-        print('Concatenating dependency on ICS {}'.format(dep))
-        subprocess.check_call(['/bin/bash', '-c', b'cat ' + newTemp + b' >> ' + final])
-        subprocess.check_call(['/bin/bash', '-c', b'echo -e "\n" >> ' + final])
+        for dep in requires:
+            spec = glob.glob('./spec/ics-' + str(dep).zfill(3) + '-*')[0] + '/README.md'
+            newTemp = extract_typescript(spec)
+            print('Concatenating dependency on ICS {}'.format(dep))
+            subprocess.check_call(['/bin/bash', '-c', b'cat ' + newTemp + b' >> ' + final])
+            subprocess.check_call(['/bin/bash', '-c', b'echo -e "\n" >> ' + final])
 
-    subprocess.check_call(['/bin/bash', '-c', b'cat ' + temp + b' >> ' + final])
+        subprocess.check_call(['/bin/bash', '-c', b'cat ' + temp + b' >> ' + final])
 
-    res = subprocess.run(['tsc', '--lib', 'es6', '--downlevelIteration', final])
+        res = subprocess.run(['tsc', '--lib', 'es6', '--downlevelIteration', final])
 
-    if res.returncode != 0:
-        print(res)
-        # sys.exit(1)
+        if res.returncode != 0:
+            print(res)
+            # sys.exit(1)
 
     subprocess.check_call(['rm', temp])
