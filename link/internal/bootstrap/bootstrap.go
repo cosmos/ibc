c700754b062b2f1aaf1ad8ea1fb6aa2313bcdad3
Dmitry S <11892559+swift1337@users.noreply.github.com>
2026-07-07 16:26:46 +0200

    run relayer w/ attestor(s) in-process

--- link/internal/bootstrap/bootstrap.go
+++ link/internal/bootstrap/bootstrap.go
@@ -14,14 +14,19 @@ import (
 // Services is an outcome of IBC Link wiring (dep inject)
 type Services struct {
 	Context context.Context
-	Server  *server.Server
-	Store   store.Store
 	Logger  *slog.Logger
+	Server  *server.Server
+
+	Store store.Store
+
+	RelayerService  *relayer.Service
+	AttestorService *attestor.Service
 }
 
 // BuildRelayer converts config into a runnable relayer process with all of the deps provisioned
 func BuildRelayer(cfg config.Config) (*Services, error) {
 	ctx := context.Background()
+	logger := slog.With("module", "bootstrap")
 
 	// Storage
 	db, err := store.NewStore(ctx, cfg)
@@ -39,39 +44,66 @@ func BuildRelayer(cfg config.Config) (*Services, error) {
 	srv := server.New(cfg.GRPC.ListenAddress, true)
 	srv.Register(relayerHandler)
 
-	// FUTURE: if config has .attestor.attestations has length > 0,
-	// then also provision attestor GRPC!
-	// A single grpc port would contain two services: relayer and attestor.
+	services := &Services{
+		Context:         ctx,
+		Logger:          logger,
+		Server:          srv,
+		Store:           db,
+		RelayerService:  relayerService,
+		AttestorService: nil,
+	}
 
-	return &Services{
-		Context: ctx,
-		Server:  srv,
-		Store:   db,
-		Logger:  slog.With("module", "bootstrap"),
-	}, nil
+	// Dual mode: if .attestor config is provided, then we can run both relayer and attestor in the same process.
+	// This might be useful for PoC/testing environments or when an operator wants to run the relayer
+	// and one of attestors in the same process
+	if len(cfg.Attestor.Attestations) > 0 {
+		logger.Info("Attestor config provided, running in dual mode: relayer with attestor")
+
+		attestorService, attestorHandler, err := buildAttestor(cfg)
+		if err != nil {
+			return nil, err
+		}
+
+		services.AttestorService = attestorService
+		srv.Register(attestorHandler)
+	}
+
+	return services, nil
 }
 
 // BuildAttestors converts config into a runnable attestor process with all of the deps provisioned
 func BuildAttestor(cfg config.Config) (*Services, error) {
 	ctx := context.Background()
+	logger := slog.With("module", "bootstrap")
 
-	// Services
-	attestorService, err := attestor.NewFromConfig(cfg)
+	attestorService, attestorHandler, err := buildAttestor(cfg)
 	if err != nil {
 		return nil, err
 	}
-
-	// Handlers
-	attestorHandler := server.NewAttestorHandler(attestorService)
 
 	// Server
 	srv := server.New(cfg.GRPC.ListenAddress, true)
 	srv.Register(attestorHandler)
 
 	return &Services{
-		Context: ctx,
-		Server:  srv,
-		Store:   nil, // attestor is stateless
-		Logger:  slog.With("module", "bootstrap"),
+		Context:         ctx,
+		Logger:          logger,
+		Server:          srv,
+		Store:           nil, // attestor is stateless
+		RelayerService:  nil,
+		AttestorService: attestorService,
 	}, nil
+}
+
+func buildAttestor(cfg config.Config) (*attestor.Service, *server.AttestorHandler, error) {
+	// Services
+	attestorService, err := attestor.NewFromConfig(cfg)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// Handlers
+	attestorHandler := server.NewAttestorHandler(attestorService)
+
+	return attestorService, attestorHandler, nil
 }
