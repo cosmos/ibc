11e5c125caf9fb83ea8753654b49033350b5b422
Dmitry S <11892559+swift1337@users.noreply.github.com>
2026-07-07 18:32:57 +0200

    simplify graceful + add tests

--- link/internal/pkg/graceful/graceful.go
+++ link/internal/pkg/graceful/graceful.go
@@ -17,6 +17,14 @@ import (
 // ShutdownFunc is a callback-type for registering callbacks before application shutdown.
 type ShutdownFunc func() error
 
+type shutdownHandler struct {
+	stop            chan os.Signal
+	mutex           sync.Mutex
+	callbacks       []ShutdownFunc
+	shutdownTimeout time.Duration
+	signalNotify    func(chan<- os.Signal, ...os.Signal)
+}
+
 const shutdownTimeout = 10 * time.Second
 
 // Shutdown errors
@@ -25,21 +33,21 @@ var (
 	ErrTimeoutExceeded = errors.New("shutdown timeout exceeded")
 )
 
-var handler *shutdownHandler
+var globalHandler *shutdownHandler
 
 func init() {
-	setupHandler()
+	setupGlobalHandler()
 }
 
-func setupHandler() {
+func setupGlobalHandler() {
 	notify := make(chan os.Signal, 1)
 	signal.Notify(notify, syscall.SIGINT, syscall.SIGTERM)
-	handler = newHandler(notify)
+	globalHandler = newHandler(notify)
 }
 
 // AddCallback registers a callback for execution before shutdown.
 func AddCallback(fn ShutdownFunc) {
-	handler.add(fn)
+	globalHandler.add(fn)
 }
 
 // WaitShutdown waits for application shutdown.
@@ -47,23 +55,53 @@ func AddCallback(fn ShutdownFunc) {
 // returns ErrForceShutdown if interrupted
 // returns ErrTimeoutExceeded if timeout exceeded
 func WaitShutdown() error {
+	return globalHandler.waitShutdown()
+}
+
+func newHandler(notify chan os.Signal) *shutdownHandler {
+	return &shutdownHandler{
+		stop:            notify,
+		shutdownTimeout: shutdownTimeout,
+		signalNotify:    signal.Notify,
+	}
+}
+
+func (h *shutdownHandler) add(fn ShutdownFunc) {
+	h.mutex.Lock()
+	defer h.mutex.Unlock()
+
+	h.callbacks = append(h.callbacks, fn)
+}
+
+func (h *shutdownHandler) callbackSnapshot() []ShutdownFunc {
+	h.mutex.Lock()
+	defer h.mutex.Unlock()
+
+	callbacks := make([]ShutdownFunc, len(h.callbacks))
+	copy(callbacks, h.callbacks)
+	return callbacks
+}
+
+func (h *shutdownHandler) waitShutdown() error {
 	// block until a signal is received
-	<-handler.stop
+	<-h.stop
 
 	slog.Info("Shutdown signal received")
 
-	// another channel sub to force-quite
+	// another channel sub to force-quit
 	forceNotify := make(chan os.Signal, 1)
-	signal.Notify(forceNotify, syscall.SIGINT, syscall.SIGTERM)
+	h.signalNotify(forceNotify, syscall.SIGINT, syscall.SIGTERM)
+	defer signal.Stop(forceNotify)
 
-	ctx, cancel := context.WithTimeout(context.Background(), shutdownTimeout)
+	ctx, cancel := context.WithTimeout(context.Background(), h.shutdownTimeout)
 	defer cancel()
 
+	callbacks := h.callbackSnapshot()
 	done := make(chan struct{})
 	go func() {
 		defer close(done)
-		for i := len(handler.callbacks) - 1; i >= 0; i-- {
-			err := handler.callbacks[i]()
+		for i := len(callbacks) - 1; i >= 0; i-- {
+			err := callbacks[i]()
 			if err != nil {
 				slog.Error("Shutdown callback error", "err", err)
 			}
@@ -78,22 +116,4 @@ func WaitShutdown() error {
 	case <-ctx.Done():
 		return ErrTimeoutExceeded
 	}
-}
-
-type shutdownHandler struct {
-	stop      chan os.Signal
-	mutex     sync.Mutex
-	callbacks []ShutdownFunc
-}
-
-func newHandler(notify chan os.Signal) *shutdownHandler {
-	return &shutdownHandler{
-		stop: notify,
-	}
-}
-
-func (h *shutdownHandler) add(fn ShutdownFunc) {
-	h.mutex.Lock()
-	h.callbacks = append(h.callbacks, fn)
-	h.mutex.Unlock()
 }
