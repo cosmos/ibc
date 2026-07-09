9b12aefda5b626dcfd6d7013b2aae85a59893eab
Dennis Fang <dhsfang@gmail.com>
2026-07-09 16:18:00 -0700

    move attestor config into its own file

--- link/internal/config/config.go
+++ link/internal/config/config.go
@@ -43,24 +43,6 @@ type DBConfig struct {
 	URL  string `yaml:"url"`
 }
 
-// AttestorConfig represents the entrypoint for running the process as an attestor
-type AttestorConfig struct {
-	Attestations []AttestationConfig `yaml:"attestations"`
-}
-
-// AttestationConfig represents a single attestation configuration in case when the binary
-// runs attestors. Signer is a reference to .singers section in the config (future)
-// Name should be unique within the config.
-type AttestationConfig struct {
-	ChainID string `yaml:"chainId"`
-	Name    string `yaml:"name"`
-
-	// todo: future work
-	RouterAddress  string `yaml:"-"`
-	FinalityOffset int64  `yaml:"-"`
-	Signer         string `yaml:"-"`
-}
-
 // DefaultConfig sample config using default values and Sqlite.
 func DefaultConfig() Config {
 	return Config{
@@ -231,26 +213,6 @@ func DBConfigFromURL(url string) (DBConfig, error) {
 	}
 
 	return db, db.Validate()
-}
-
-// Validate validates the attestor config. Allows empty attestations.
-func (c AttestorConfig) Validate() error {
-	set := make(map[string]struct{})
-
-	for _, attestation := range c.Attestations {
-		if attestation.ChainID == "" {
-			return errors.Errorf(".attestations chainId required")
-		}
-		if attestation.Name == "" {
-			return errors.Errorf(".attestations name required")
-		}
-		if _, ok := set[attestation.Name]; ok {
-			return errors.Errorf(".attestations duplicate name: %q", attestation.Name)
-		}
-		set[attestation.Name] = struct{}{}
-	}
-
-	return nil
 }
 
 // PrintJSON prints anything as JSON to stdout.
