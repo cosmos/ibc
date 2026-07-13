b53c4027fa1d57e537ec9a4af4c998f4f4defa27
Dennis Fang <dhsfang@gmail.com>
2026-07-13 13:13:44 -0700

    split store migrations per table

--- link/internal/store/migrations/postgres/001-packets.sql
+++ link/internal/store/migrations/postgres/001-packets.sql
@@ -31,41 +31,5 @@ create table if not exists packets (
 create unique index if not exists index_packet
     on packets (source_chain_id, packet_sequence_number, packet_source_client_id);
 
-create table if not exists relay_requests (
-    id              bigserial                PRIMARY KEY,
-    source_chain_id text                     NOT NULL,
-    source_tx_hash  text                     NOT NULL,
-    created_at      timestamp with time zone NOT NULL DEFAULT now(),
-
-    constraint relay_requests_source_tx_unique
-        unique (source_chain_id, source_tx_hash)
-);
-
-create table if not exists relayer_tx_submissions (
-    id              bigserial                          PRIMARY KEY,
-    tx_hash         text                               NOT NULL,
-    chain_id        text                               NOT NULL,
-    tx_type         text                               NOT NULL,
-    relayer_address text                               NOT NULL,
-    submitted_at    timestamp with time zone           NOT NULL DEFAULT now(),
-    resolved_at     timestamp with time zone,
-    gas_cost_amount numeric,
-    status          text                               NOT NULL,
-    execution_error text,
-
-    constraint relayer_tx_submissions_tx_unique
-        unique (chain_id, tx_hash)
-);
-
-create table if not exists packet_tx_submissions (
-    packet_id   bigint NOT NULL references packets (id),
-    submission_id bigint NOT NULL references relayer_tx_submissions (id),
-
-    primary key (packet_id, submission_id)
-);
-
 -- +migrate Down
-drop table if exists packet_tx_submissions;
-drop table if exists relayer_tx_submissions;
-drop table if exists relay_requests;
 drop table if exists packets;
