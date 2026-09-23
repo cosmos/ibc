ae39835dbb28467efc478c72f3aa44e4056d478c
Gjermund Garaba <gjermund@garaba.net>
2026-09-23 10:50:47 +0200

    fix(cli): reconcile deployment config without losing custom settings (#1468)
    * fix(cli): reconcile deployment config without losing custom settings
    
    Signed-off-by: Gjermund Garaba <gjermund@garaba.net>
    
    * fix(cli): clarify unsupported client type transitions
    
    Signed-off-by: Gjermund Garaba <gjermund@garaba.net>
    
    * docs(cli): track client type transition follow-up
    
    Signed-off-by: Gjermund Garaba <gjermund@garaba.net>
    
    * style(cli): apply configured golines formatting
    
    Signed-off-by: Gjermund Garaba <gjermund@garaba.net>
    
    ---------
    
    Signed-off-by: Gjermund Garaba <gjermund@garaba.net>

--- cli/cmd/ibc/main.go
+++ cli/cmd/ibc/main.go
@@ -170,9 +170,9 @@ func init() {
 		Uint64Var(&flagDeployTimestamp, "timestamp", 0, "initial trusted timestamp seconds (default: counterparty head)")
 
 	cmdDeployRenderConfig.Flags().
-		StringVar(&flagDeployRenderSignerA, "signer-a", "", "signers[] alias submitting relay txs on chainA")
+		StringVar(&flagDeployRenderSignerA, "signer-a", "", "Override the relay signer on chainA; omitted preserves existing settings")
 	cmdDeployRenderConfig.Flags().
-		StringVar(&flagDeployRenderSignerB, "signer-b", "", "signers[] alias submitting relay txs on chainB")
+		StringVar(&flagDeployRenderSignerB, "signer-b", "", "Override the relay signer on chainB; omitted preserves existing settings")
 	cmdDeployRenderConfig.Flags().
 		BoolVarP(&flagDeployRenderPopulate, "populate-config", "p", false, "write the printed config to the config file")
 
